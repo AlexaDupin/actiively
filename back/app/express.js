61bcbdb6edbaa836979d76fb37fb6e7e24e78b9e
Alexa Dupin <alexa.dupin@gmail.com>
2025-06-23 21:06:36 +0200

    feature(cron): added cron feature

--- back/app/express.js
+++ back/app/express.js
@@ -3,8 +3,10 @@ require('dotenv').config();
 const express = require('express');
 const router = require('./routers');
 const cors = require('cors');
+const job = require("./config/cron.js");
 
 const app = express();
+job.start();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
